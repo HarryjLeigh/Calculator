844068650b865db1a6b54129ab819bcdf8d88e5c
harryL <harryleigh@hotmail.co.uk>
2024-10-17 19:37:55 +0100

    Made some changes to the program file.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
 
 bool endApp = false;
 // Display title as the C# console calculator app.
@@ -75,36 +76,3 @@ while (!endApp)
     Console.WriteLine("\n"); // Friendly linespacing.
 }
 
-
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
